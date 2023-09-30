39fc4833b6ab6c227f09f6cc22deb45e59f0063f
20724926 <20724926@roughgarden.westernsydney.edu.au>
2023-09-30 19:43:05 +1000

    Committer: 20724926 <20724926@roughgarden.westernsydney.edu.au> On branch master Initial commit

---
+++ ResidualBlock.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+
+    def forward(self, x):
+        out = self.conv2(self.relu(self.conv1(x)))
+        out += x
+        out = self.relu(out)
+        return out
